f8eb8ba38989e1b69433e5521e9dc1326b3b09cb
shamith777 <142828883+shamith777@users.noreply.github.com>
2025-11-29 11:20:56 +0530

    Update cie2.py

--- cie2.py
+++ cie2.py
@@ -6,10 +6,10 @@ if len(sys.argv) !=5:
     dept = sys.argv[3]
     email = sys.argv[4]
 else:
-    name = "Shamith"
-    reg_no = 272
+    name = "Abhi"
+    reg_no = 277
     dept = "BCA"
-    email = "shamithramdurg302@gmail.com"
+    email = "abhi@gmail.com"
 
 print("Student Name:",name)
 print("Registration Number:",reg_no)
