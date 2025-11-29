0d7cbd579e8c93bb47ed26c7e755cee4c45fadae
shamith777 <shamithramdurg302@gmail.com>
2025-11-29 10:35:41 +0530

    Saving File

---
+++ cie2.py
@@ -0,0 +1,17 @@
+import sys
+if len(sys.argv) !=5:
+    script_name = sys.argv[0]
+    name = sys.argv[1]
+    reg_no = sys.argv[2]
+    dept = sys.argv[3]
+    email = sys.argv[4]
+else:
+    name = "Shamith"
+    reg_no = "272"
+    dept = "BCA"
+    email = "shamithramdurg@gmail.com"
+
+print("Student Name:",name)
+print("Registration Number:",reg_no)
+print("Department:",dept)
+print("Email ID:",email)
